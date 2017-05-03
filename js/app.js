61a15427769d6bb7bc1114096565a50bd18851bc
Shazil Tovar <shazil@gmail.com>
2017-05-03 16:14:00 -0500

    finalizado con modal

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
 /*
- * Archivo principal de funcionalidad de JS
- */
+ * Archivo principal de funcionalidad de JS*/
+ alert("hola");
+ var img = document.getElementsByClassName("imagen");
+ var lengthImg = img.length;
+
+ for (var i = 0; i<lengthImg;i++) {
+ 	img[i].addEventListener('click', openModal)
+ }
+
+ function openModal(){
+ 	document.getElementById("imgModal").style.display = "block";
+ 	document.getElementById("imagenZoom").src= this.src
+ }
+
+ var span = document.getElementsByClassName("cerrar")[0];
+ span.onclick = function() {
+     document.getElementById("imgModal").style.display = "none";
+ }
